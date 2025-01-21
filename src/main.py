2ca38d0963afcfc6609ab3384a27a3eed500674d
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-01-21 14:54:06 +1300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    text_node = TextNode("test", TextType.NORMAL_TEXT, "test")
+    print(text_node)
+
+
+if __name__ == "__main__":
+    main()
